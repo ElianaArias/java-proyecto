fa31c40c83c24fa92e48bcd2be94956c8493e9e9
Eliana Arias <ariaselianamariel@gmail.com>
2024-11-13 16:22:45 -0300

    Agrego carpetas postman y scripts

---
+++ ventaOnline/scripts/FacturacionSegundaEntrega-Arias.sql
@@ -0,0 +1,42 @@
+create database if not exists coderhouse;
+use coderhouse;
+select*from cliente;
+/*
+INSERT INTO cliente (apellido, dni, mail, nombre)
+VALUES 
+('Arias', '11111111', 'eliana@gmail.com', 'Eliana'),
+('Alba', '22222222', 'alejandro@gmail.com', 'Alejandro'),
+('Lezama', '33333333', 'bruno@gmail.com', 'Bruno'),
+('Diorio', '44444444', 'federico@gmail.com', 'Federico'),
+('Brunetti', '55555555', 'agustin@gmail.com', 'Agustin');
+
+INSERT INTO producto (marca, nombre, precio, stock)
+VALUES 
+('Dell', 'Laptop', 1200.00, 10),
+('Logitech', 'Mouse', 25.00, 20),
+('HP', 'Teclado', 45.00, 30),
+('Dell', 'Monitor', 600.00, 10),
+('HP', 'Notebook', 1300.00, 10);
+select*from producto;
+
+INSERT INTO venta (fecha_venta, precio_total, cliente_id)
+VALUES 
+('06-06-2024', 1200.00, 1),
+('07-07-2024', 600.00, 2),
+('09-09-2024', 25.00, 4),
+('10-10-2024', 45.00, 3);
+select*from venta;
+
+INSERT INTO venta_detalle (cantidad, precio, producto_id, venta_id)
+VALUES
+(1, 1200.00, 1, 1),
+(1, 600.00, 4, 2),
+(1, 25.00, 2, 3),
+(1, 45.00, 3, 4);
+select*from venta_detalle;
+*/
+
+
+
+
+
